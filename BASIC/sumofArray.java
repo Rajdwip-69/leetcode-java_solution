f7094ca5b13eb74c91504a51a5965171ffbf0880
Rajdwip-69 <rajdwipghosh27@gmail.com>
2024-07-14 09:14:35 +0530

    this is all dsa java code

---
+++ BASIC/sumofArray.java
@@ -0,0 +1,13 @@
+public class sumofArray{
+    // /**
+    //  * @param args
+    //  */
+    public static void main(String[]args){
+  int nums [] = {2,45,4,7};
+ int sum = 0;
+ for(int num : nums){
+    sum = sum+num;
+ }
+ System.out.println("The Result is:"+sum);
+}
+}
